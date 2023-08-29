1c2e38cd7bb85f12f02ba282d8b519c02b5e79d0
Bibin Prakash <bibin.dev2000@gmail.com>
2023-08-29 15:02:53 -0400

    removed unused imports

--- app/spotify-to-apple/songDetails/columns.tsx
+++ app/spotify-to-apple/songDetails/columns.tsx
@@ -1,7 +1,11 @@
 "use client";
-
+import { useState } from "react";
 import { ColumnDef } from "@tanstack/react-table";
 import { Checkbox } from "@/components/ui/checkbox";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Clipboard } from "lucide-react";
+
 export type Song = {
   id: string;
   trackName: string;
@@ -31,7 +35,17 @@ export const columns: ColumnDef<Song>[] = [
   },
   {
     accessorKey: "trackName",
-    header: "Track Name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "trackArtist",
@@ -40,5 +54,28 @@ export const columns: ColumnDef<Song>[] = [
   {
     accessorKey: "trackURL",
     header: "Apple Music Link",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const song = row.original;
+      const [copied, setCopied] = useState(false);
+      const handleCopyClick = (url: string) => {
+        navigator.clipboard.writeText(url);
+        setCopied(true);
+        setTimeout(() => {
+          setCopied(false);
+        }, 1500);
+      };
+      return (
+        <Button
+          className="flex justify-center"
+          variant="ghost"
+          onClick={() => handleCopyClick(song.trackURL)}
+        >
+          {copied ? "copied" : <Clipboard className="h-4 w-4" />}
+        </Button>
+      );
+    },
   },
 ];
